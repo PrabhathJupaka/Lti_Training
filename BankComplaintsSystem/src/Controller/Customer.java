0bf77ec259c9fe0e8ff7c5a7aee284a1beb625b6
User <User@DESKTOP-7O7MS36.bbrouter>
2022-07-13 12:42:54 +0530

    first commit

---
+++ BankComplaintsSystem/src/Controller/Customer.java
@@ -0,0 +1,19 @@
+package Controller;
+
+public class Customer {
+	int customer_id;
+	String customer_name;
+	public int getCustomer_id() {
+		return customer_id;
+	}
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	public String getCustomer_name() {
+		return customer_name;
+	}
+	public void setCustomer_name(String customer_name) {
+		this.customer_name = customer_name;
+	}
+
+}
